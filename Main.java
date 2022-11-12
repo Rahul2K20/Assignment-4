f0eef27fb9db19997452a681f176bce144f6e087
Rahul Nagarajan <63788480+Rahul2K20@users.noreply.github.com>
2022-11-11 22:27:48 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main
 {
     public static void main( String[] args )
     {
-        IndividualHolder obj= new IndividualHolder(AccountHolder.getNextID(), "3400 Poly Vista", "Student", "PQRS1234");
+        IndividualHolder obj = new IndividualHolder(AccountHolder.getNextID(), "3400 Poly Vista", "Student", "PQRS1234");
 		Account acc = new Account(5000, obj);
 		
 		System.out.println("The current Balance is: " +acc.getBalance());
